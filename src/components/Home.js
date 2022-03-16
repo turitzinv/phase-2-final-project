9e9ae04446d88189e2bbbe869a9819a0994ee4e0
Victor Turitzin <turitzinv@gmail.com>
2022-03-15 21:17:08 -0400

    Added an Imgs folder to hold the luggage image. Luggage img was added to Home component.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,18 @@
 import React from "react";
 
 function Home() {
-  return <h1>Home</h1>;
+  return (
+    <div>
+      <h1>Home</h1>
+      <img 
+      id="luggage" 
+      src ="https://user-images.githubusercontent.com/93049794/158497489-7ed7c6bb-6fc1-4c55-ae78-f6031abee85c.jpg"
+      alt="luggage"
+      width = "500"
+      height = "500"
+      />
+    </div>
+  );
 }
 
 export default Home;
