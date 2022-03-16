6f66cf04a1cccac7754ccdc33d52a2f4a544ebf9
Victor Turitzin <turitzinv@gmail.com>
2022-03-15 20:40:41 -0400

    Updated NavBar to use NavLink with exact path and styling. Also updated App to inclue the routes from NavBar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 // import logo from './logo.svg';
 // import './App.css';
-import React from 'react';
+import React from "react";
+import { Route } from "react-router-dom"; //Switch not used due to error
+import NavBar from "./components/NavBar";
+import Home from "./components/Home";
+import NewItemForm from "./components/NewItemForm";
+import CurrentPackingList from "./components/CurrentPackingList";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div>
+      <NavBar />
+      <Route exact path="/home">
+        <Home />
+      </Route>
+      <Route exact path="/newitemform">
+        <NewItemForm />
+      </Route>
+      <Route exact path="/currentpackinglist">
+        <CurrentPackingList />
+      </Route>
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,15 +1,41 @@
-import React from 'react'
-import {NavLink} from "react-router-dom";
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const linkStyles = {
+  display: "inline-block",
+  width: "50px",
+  padding: "12px",
+  margin: "0 6px 6px",
+  background: "blue",
+  textDecoration: "none",
+  color: "white",
+};
 
 function NavBar() {
   return (
-    <div className="navbar">
-     
-
-    </div>
-  )
+  <div>
+      <NavLink
+        to="/home"
+        style={linkStyles}
+        >
+          Home
+      </NavLink>
+      <NavLink
+        to="/newitemform"
+        style={linkStyles}
+        >
+          New Item Form
+      </NavLink>
+      <NavLink
+        to="/currentpackinglist"
+        style={linkStyles}
+        >
+          Current Packing List
+      </NavLink>
+  </div>
+  );
 }
 
 export default NavBar;
 
-//NavLinks for Home, New Item Form, Current Packing List, possibly Filter
+//NavLinks for Home, New Item Form, Current Packing List
