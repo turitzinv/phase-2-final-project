be939cab60c3725acc01fe46b78defd77178b193
Victor Turitzin <turitzinv@gmail.com>
2022-03-17 12:28:52 -0400

    Added a table to CurrentPackingList component and began creating the form for New Item Form.

--- src/components/NewItemForm.js
+++ src/components/NewItemForm.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from "react";
 
-function NewItemForm () {
+function NewItemForm() {
   return (
     <div>
-
+      <h1>Submit New Packing Item Below</h1>
+      <form>
+        <div class="input-field">
+          <input id="item" type="text" />
+          <label for="item"> Item</label>
+        </div>
+        <div class="input-field">
+          <input id="category" type="text" />
+          <label for="category"> Category</label>
+        </div>
+      </form>
     </div>
-  )
+  );
 }
 
 export default NewItemForm;
