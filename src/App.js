ab190a3542aca9c1f718698304bdb4d6be9cfce6
Victor Turitzin <turitzinv@gmail.com>
2022-04-03 10:47:04 -0400

    Cleaned up App.js code to look cleaner.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
-// import logo from './logo.svg';
-// import './App.css';
-import React, {useState, useEffect} from "react";
-import { Route, Switch } from "react-router-dom"; 
+import React, { useState, useEffect } from "react";
+import { Route, Switch } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import Home from "./components/Home";
 import NewItemForm from "./components/NewItemForm";
@@ -9,17 +7,13 @@ import CurrentPackingList from "./components/CurrentPackingList";
 
 function App() {
   const [allItems, setItems] = useState([]);
-  const [selectedCategory, setSelectedCategory] = useState("All")
+  const [selectedCategory, setSelectedCategory] = useState("All");
 
-function handleSelectCategory(event) {
-  setSelectedCategory(event.target.value)
-}
+  const categoryList = allItems.map((item) => {
+    return item.category;
+  });
 
-const categoryList = allItems.map((item) => {
-  return item.category;
-});
-
-let uniqueCategoryList = ["All"];
+  let uniqueCategoryList = ["All"];
   categoryList.forEach((category) => {
     if (!uniqueCategoryList.includes(category)) {
       uniqueCategoryList.push(category);
@@ -27,13 +21,13 @@ let uniqueCategoryList = ["All"];
   });
 
   const displayCategoryList = uniqueCategoryList.map((category, index) => {
-      return <option key={index}>{category}</option>
-  })
+    return <option key={index}>{category}</option>;
+  });
 
   useEffect(() => {
     fetch("http://localhost:3001/list")
-    .then((resp) => resp.json())
-    .then((items) => setItems(items))
+      .then((resp) => resp.json())
+      .then((items) => setItems(items));
   }, []);
 
   function handleAddingItem(newItem) {
@@ -41,17 +35,19 @@ let uniqueCategoryList = ["All"];
   }
 
   function handleDeleteItem(deletedItem) {
-    const updatedItems = allItems.filter((item) => item.id !== deletedItem.id)
-    setItems(updatedItems)
+    const updatedItems = allItems.filter((item) => item.id !== deletedItem.id);
+    setItems(updatedItems);
+  }
+
+  function handleSelectCategory(event) {
+    setSelectedCategory(event.target.value);
   }
 
   function filteredCategory() {
-    if (selectedCategory ==="All"){
-      return allItems
+    if (selectedCategory === "All") {
+      return allItems;
     } else {
-      return allItems.filter((item) =>
-        item.category === selectedCategory
-      )
+      return allItems.filter((item) => item.category === selectedCategory);
     }
   }
 
@@ -59,26 +55,24 @@ let uniqueCategoryList = ["All"];
     <div>
       <NavBar />
       <Switch>
-      <Route exact path="/">
-        <Home />
-      </Route>
-      <Route exact path="/newitemform">
-        <NewItemForm onAddingItem={handleAddingItem} />
-      </Route>
-      <Route exact path="/currentpackinglist">
-        <CurrentPackingList 
-        allItems={filteredCategory()} 
-        handleDeleteItem={handleDeleteItem}
-        handleSelectCategory={handleSelectCategory}
-        selectedCategory={selectedCategory}
-        displayCategoryList={displayCategoryList}
-        // handleSearchFilter={handleSearchFilter}
-        // search={search}
-        />
-      </Route>
-      <Route path="/*" >
-      <h1>404</h1>
-      </Route>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/newitemform">
+          <NewItemForm onAddingItem={handleAddingItem} />
+        </Route>
+        <Route path="/currentpackinglist">
+          <CurrentPackingList
+            allItems={filteredCategory()}
+            handleDeleteItem={handleDeleteItem}
+            handleSelectCategory={handleSelectCategory}
+            selectedCategory={selectedCategory}
+            displayCategoryList={displayCategoryList}
+          />
+        </Route>
+        <Route path="/*">
+          <h1>404</h1>
+        </Route>
       </Switch>
     </div>
   );
